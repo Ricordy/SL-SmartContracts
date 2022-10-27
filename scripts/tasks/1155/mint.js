9baef978578f2670b17f04c9572417fb3c42240a
Ricordy <youberrody@gmail.com>
2022-10-27 22:51:14 +0100

    Feature: Working copy of major functions. Still on low level requirements

--- scripts/tasks/1155/mint.js
+++
@@ -1,6 +0,0 @@
-const hre = require("hardhat")
-import { task } from "hardhat/config";
-
-task("mint", "try to mint 10 tokens").addPositionalParam("address").setAction(async (args) => {
-    console.log(args)
-});
