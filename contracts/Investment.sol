e37fd1fe0ecf0964507247bbf2e8513284ad9abc
Ricordy <youberrody@gmail.com>
2023-06-19 16:50:21 +0100

    Feature: SafeERC20 implemented in Investment Contract

--- contracts/Investment.sol
+++ contracts/Investment.sol
@@ -2,25 +2,24 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
-
 
 /// Investing amount exceeded the maximum allowed
 /// @param amount the amount user is trying to invest
 /// @param maxAllowed max amount allowed to invest
 error InvestmentExceedMax(uint256 amount, uint256 maxAllowed);
 
-interface ISLCore{
-    function whichLevelUserHas(
-        address user
-    ) external view returns(uint);
-
+interface ISLCore {
+    function whichLevelUserHas(address user) external view returns (uint);
 }
 
-contract Investment is ERC20, Ownable, ReentrancyGuard {
+interface IToken is IERC20 {}
 
+contract Investment is ERC20, Ownable, ReentrancyGuard {
+    using SafeERC20 for IERC20;
     ///
     //-----STATUS------
     ///
@@ -45,43 +44,31 @@ contract Investment is ERC20, Ownable, ReentrancyGuard {
     uint8 public immutable CONTRACT_LEVEL;
     mapping(address => bool) public userWithdrawed;
 
-
     ///
     //-----EVENTS------
     ///
-    event UserInvest (
-        address user,
-        uint256 amount,
-        uint256 time
-    );
+    event UserInvest(address user, uint256 amount, uint256 time);
 
-    event SLWithdraw (
-        uint256 amount,
-        uint256 time
-    );
+    event SLWithdraw(uint256 amount, uint256 time);
 
-    event Withdraw (
-        address user,
-        uint256 amount,
-        uint256 time
-    );
+    event Withdraw(address user, uint256 amount, uint256 time);
 
-    event ContractRefilled (
-        uint256 amount,
-        uint256 profit,
-        uint256 time
-    );
+    event ContractRefilled(uint256 amount, uint256 profit, uint256 time);
 
-    event ContractFilled (
-        uint256 time
-    );
+    event ContractFilled(uint256 time);
 
     ///
     //-----CONSTRUCTOR------
     ///
-    constructor(uint256 _totalInvestment, address _entryNFTAddress, address _paymentTokenAddress, uint8 _contractLevel) ERC20("InvestmentCurrency", "IC"){
+    constructor(
+        uint256 _totalInvestment,
+        address _entryNFTAddress,
+        address _paymentTokenAddress,
+        uint8 _contractLevel
+    ) ERC20("InvestmentCurrency", "IC") {
         require(
-            _entryNFTAddress != address(0) && _paymentTokenAddress != address(0),
+            _entryNFTAddress != address(0) &&
+                _paymentTokenAddress != address(0),
             "Investment: Check the parameters and redeploy"
         );
         totalInvestment = _totalInvestment * 10 ** decimals();
@@ -97,10 +84,12 @@ contract Investment is ERC20, Ownable, ReentrancyGuard {
     function invest(uint256 _amount) public nonReentrant isAllowed isProgress {
         require(_amount >= MINIMUM_INVESTMENT, "Not enough amount to invest");
 
-        uint256 userInvested = _amount * 10 ** decimals() + balanceOf(msg.sender);
+        uint256 userInvested = _amount *
+            10 ** decimals() +
+            balanceOf(msg.sender);
         uint256 maxToInvest = getMaxToInvest();
-        
-        if ( userInvested > maxToInvest) {
+
+        if (userInvested > maxToInvest) {
             revert InvestmentExceedMax(userInvested, maxToInvest);
         }
         if (totalSupply() + _amount * 10 ** 6 == totalInvestment) {
@@ -110,23 +99,35 @@ contract Investment is ERC20, Ownable, ReentrancyGuard {
 
         _mint(msg.sender, _amount * 10 ** decimals());
         investors++;
+
         ERC20 _token = ERC20(paymentTokenAddress);
-        require(_token.transferFrom(msg.sender, address(this), _amount *  10 ** _token.decimals()), "Puzzle: Error in token transfer");
-        
-        
+        IERC20(paymentTokenAddress).safeTransferFrom(
+            msg.sender,
+            address(this),
+            _amount * 10 ** _token.decimals()
+        );
+
         emit UserInvest(msg.sender, _amount, block.timestamp);
     }
 
-    function withdraw() external nonReentrant isNotPaused isAllowed isWithdrawOrRefunding {
-        require(!userWithdrawed[msg.sender], "Investment: User already withdrawed");
+    function withdraw()
+        external
+        nonReentrant
+        isNotPaused
+        isAllowed
+        isWithdrawOrRefunding
+    {
+        require(
+            !userWithdrawed[msg.sender],
+            "Investment: User already withdrawed"
+        );
         userWithdrawed[msg.sender] = true;
-        
+
         ERC20 _token = ERC20(paymentTokenAddress);
         uint256 finalAmount = calculateFinalAmount(balanceOf(msg.sender));
-        
-        require( _token.transfer(msg.sender, finalAmount), "Puzzle: Error in token transfer");
 
-        
+        IERC20(paymentTokenAddress).safeTransfer(msg.sender, finalAmount);
+
         emit Withdraw(msg.sender, finalAmount, block.timestamp);
     }
 
@@ -134,32 +135,55 @@ contract Investment is ERC20, Ownable, ReentrancyGuard {
         uint256 totalBalance;
         ERC20 _token = ERC20(paymentTokenAddress);
         totalBalance = totalContractBalanceStable();
-    
-        require(totalBalance >= totalInvestment * 80 / 100 , "Total not reached"); 
-        require(_token.balanceOf(address(this)) >= totalInvestment * 80 / 100 , "Total not reached"); //Maybe to be removed 
+
+        require(
+            totalBalance >= (totalInvestment * 80) / 100,
+            "Total not reached"
+        );
+        require(
+            _token.balanceOf(address(this)) >= (totalInvestment * 80) / 100,
+            "Total not reached"
+        ); //Maybe to be removed
 
         emit SLWithdraw(totalBalance, block.timestamp);
-        require(_token.transfer(msg.sender, totalContractBalanceStable()), "Puzzle: Error in token transfer");
-    
+
+        IERC20(paymentTokenAddress).safeTransfer(
+            msg.sender,
+            totalContractBalanceStable()
+        );
     }
 
-    function refill(uint256 _amount, uint256 _profitRate) public nonReentrant onlyOwner isAllowed isProcess isNotPaused {
+    function refill(
+        uint256 _amount,
+        uint256 _profitRate
+    ) public nonReentrant onlyOwner isAllowed isProcess isNotPaused {
         ERC20 _token = ERC20(paymentTokenAddress);
-        require(totalInvestment + (totalInvestment * _profitRate /100) == _amount * 10 **  _token.decimals() , "Not correct value");
+        require(
+            totalInvestment + ((totalInvestment * _profitRate) / 100) ==
+                _amount * 10 ** _token.decimals(),
+            "Not correct value"
+        );
 
-        
         returnProfit = _profitRate;
         // Change status to withdraw
         _changeStatus(Status.Withdraw);
 
-        require(_token.transferFrom(msg.sender, address(this), _amount *  10 ** _token.decimals()), "Puzzle: Error in token transfer");
+        IERC20(paymentTokenAddress).transferFrom(
+            msg.sender,
+            address(this),
+            _amount * 10 ** _token.decimals()
+        );
         emit ContractRefilled(_amount, _profitRate, block.timestamp);
     }
 
     ///
     //-----GETTERS------
     ///
-    function totalContractBalanceStable() public view returns(uint256 totalBalance) {
+    function totalContractBalanceStable()
+        public
+        view
+        returns (uint256 totalBalance)
+    {
         totalBalance = totalSupply(); //check is totalSuppkly burns when burn is called
     }
 
@@ -170,13 +194,15 @@ contract Investment is ERC20, Ownable, ReentrancyGuard {
         }
     }
 
-    function calculateFinalAmount(uint256 _amount) internal view returns(uint256 totalAmount) {
-        totalAmount = (_amount + (_amount * returnProfit / 100));
+    function calculateFinalAmount(
+        uint256 _amount
+    ) internal view returns (uint256 totalAmount) {
+        totalAmount = (_amount + ((_amount * returnProfit) / 100));
     }
 
-    /// 
+    ///
     //---- MODIFIERS------
-    /// 
+    ///
     modifier isNotPaused() {
         require(status != Status.Pause, "Contract paused");
         _;
@@ -193,7 +219,10 @@ contract Investment is ERC20, Ownable, ReentrancyGuard {
     }
 
     modifier isWithdrawOrRefunding() {
-        require(status == Status.Withdraw || status == Status.Refunding, "Not on Withdraw or Refunding");
+        require(
+            status == Status.Withdraw || status == Status.Refunding,
+            "Not on Withdraw or Refunding"
+        );
         _;
     }
 
@@ -203,13 +232,17 @@ contract Investment is ERC20, Ownable, ReentrancyGuard {
     }
 
     modifier isAllowed() {
-        require(ISLCore(entryNFTAddress).whichLevelUserHas(msg.sender) >= CONTRACT_LEVEL, "User does not have the required level NFT");
+        require(
+            ISLCore(entryNFTAddress).whichLevelUserHas(msg.sender) >=
+                CONTRACT_LEVEL,
+            "User does not have the required level NFT"
+        );
         _;
     }
 
-    /// 
+    ///
     //----STATUS FUNCTIONS------
-    /// 
+    ///
     function changeStatus(Status _newStatus) public onlyOwner nonReentrant {
         status = _newStatus;
     }
@@ -218,11 +251,11 @@ contract Investment is ERC20, Ownable, ReentrancyGuard {
         status = _newStatus;
     }
 
-    /// 
+    ///
     //----OVERRIDES------
-    /// 
+    ///
 
-    function decimals() override public pure returns(uint8){
+    function decimals() public pure override returns (uint8) {
         return 6;
     }
 
@@ -234,10 +267,10 @@ contract Investment is ERC20, Ownable, ReentrancyGuard {
         return false;
     }
 
-    function transfer(address to, uint256 amount) public override returns (bool) {
+    function transfer(
+        address to,
+        uint256 amount
+    ) public override returns (bool) {
         return false;
     }
-
-
-    
 }
