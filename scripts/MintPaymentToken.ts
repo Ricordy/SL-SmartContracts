208596225e8cf6b7cf6fc4a2ce1f8f0bc7fabeb9
Barrocz <youberrody@gmail.com>
2023-10-25 11:51:47 +0100

    finalized scripts

--- scripts/MintPaymentToken.ts
+++ scripts/MintPaymentToken.ts
@@ -2,9 +2,9 @@ import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { ethers } from "hardhat";
 
 import { CoinTest, CoinTest__factory } from "../typechain-types";
+import addresses from "../utils/addresses";
 
-const paymentTokenAddress: any = "0x5FbDB2315678afecb367f032d93F642f64180aa3",
-  investmentValue: number = 1000;
+const investmentValue: number = 1000;
 
 async function main() {
   const accounts: SignerWithAddress[] = await ethers.getSigners();
@@ -13,8 +13,9 @@ async function main() {
 
   const paymentTokenFactory = new CoinTest__factory(owner);
 
-  const paymentTokenContract: CoinTest =
-    paymentTokenFactory.attach(paymentTokenAddress);
+  const paymentTokenContract: CoinTest = paymentTokenFactory.attach(
+    addresses.paymentTokenAddress0
+  );
 
   console.log(`Minting ${investmentValue} tokens to Investor1: `);
   await paymentTokenContract.connect(firstInvestor).mint(investmentValue);
